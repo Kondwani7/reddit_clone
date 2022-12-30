a26c5ca5eadb547f34b516f10baeeb7648f9a686
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-12-30 17:33:41 +0200

    logo on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
-
+import { SiReddit } from 'react-icons/si'
+import Image from 'next/image'
 function Header() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className='relative h-20 w-28 flex-shrink-0 cursor-pointer'>
+            <div>
+                <Image alt="logo" width={200} height={200} objectFit='contain' src='/img/reddit.png'  />
+            </div>
+        </div>
+    )
 }
 
 export default Header
